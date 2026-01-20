b421f83325f77bc03eb62508b344248afaad1cdf
Thatcher <thatcherwer120@gmail.com>
2026-01-20 14:05:39 -0600

    Began work on Issue #2
    Created `TodoController.java` and `Todo.java` to handle todo. Currently does not show up on the localhost.
    
    Co-authored-by: Elijah Slinden <slind054@morris.umn.edu>

---
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -0,0 +1,177 @@
+package umm3601.todo;
+
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.regex;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+import org.bson.Document;
+import org.bson.UuidRepresentation;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+import org.mongojack.JacksonMongoCollection;
+
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+import com.mongodb.client.model.Sorts;
+import com.mongodb.client.result.DeleteResult;
+
+import io.javalin.Javalin;
+import io.javalin.http.BadRequestResponse;
+import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
+import io.javalin.http.NotFoundResponse;
+import umm3601.Controller;
+
+/**
+ * Controller that manages requests for info about todos.
+ */
+public class TodoController implements Controller {
+
+  private static final String API_TODOS = "/api/todos";
+  private static final String API_TODOS_BY_ID = "/api/todos/{id}";
+  static final String OWNER_KEY = "owner";
+  static final String STATUS_KEY = "status";
+  static final String BODY_KEY = "body";
+  static final String CATEGORY_KEY = "category";
+  static final String SORT_ORDER_KEY = "sortorder";
+
+  /*private static final int REASONABLE_AGE_LIMIT = 150;
+  private static final String ROLE_REGEX = "^(admin|editor|viewer)$";
+  public static final String EMAIL_REGEX = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";*/
+
+  private final JacksonMongoCollection<Todo> todosCollection;
+
+  /**
+   * Construct a controller for users.
+   *
+   * @param database the database containing user data
+   */
+  public TodoController(MongoDatabase database) {
+    todosCollection = JacksonMongoCollection.builder().build(
+        database,
+        "todos",
+        Todo.class,
+        UuidRepresentation.STANDARD);
+  }
+
+  /**
+   * Set the JSON body of the response to be the single user
+   * specified by the `id` parameter in the request
+   *
+   * @param ctx a Javalin HTTP context
+   */
+  public void getTodo(Context ctx) {
+    String id = ctx.pathParam("id");
+    Todo todo;
+
+    try {
+      todo = todosCollection.find(eq("_id", new ObjectId(id))).first();
+    } catch (IllegalArgumentException e) {
+      throw new BadRequestResponse("The requested todo id wasn't a legal Mongo Object ID.");
+    }
+    if (todo == null) {
+      throw new NotFoundResponse("The requested todo was not found");
+    } else {
+      ctx.json(todo);
+      ctx.status(HttpStatus.OK);
+    }
+  }
+
+  /**
+   * Set the JSON body of the response to be a list of all the users returned from the database
+   * that match any requested filters and ordering
+   *
+   * @param ctx a Javalin HTTP context
+   */
+  public void getTodos(Context ctx) {
+    //Bson combinedFilter = constructFilter(ctx);
+    //Bson sortingOrder = constructSortingOrder(ctx);
+
+    // All three of the find, sort, and into steps happen "in parallel" inside the
+    // database system. So MongoDB is going to find the users with the specified
+    // properties, return those sorted in the specified manner, and put the
+    // results into an initially empty ArrayList.
+    ArrayList<Todo> matchingTodos = ((MongoIterable<Todo>) todosCollection)
+      //.find(combinedFilter)
+      //.sort(sortingOrder)
+      .into(new ArrayList<>());
+
+    // Set the JSON body of the response to be the list of users returned by the database.
+    // According to the Javalin documentation (https://javalin.io/documentation#context),
+    // this calls result(jsonString), and also sets content type to json
+    ctx.json(matchingTodos);
+
+    // Explicitly set the context status to OK
+    ctx.status(HttpStatus.OK);
+  }
+
+
+  /**
+   * Utility function to generate the md5 hash for a given string
+   *
+   * @param str the string to generate a md5 for
+   */
+  public String md5(String str) throws NoSuchAlgorithmException {
+    MessageDigest md = MessageDigest.getInstance("MD5");
+    byte[] hashInBytes = md.digest(str.toLowerCase().getBytes(StandardCharsets.UTF_8));
+
+    StringBuilder result = new StringBuilder();
+    for (byte b : hashInBytes) {
+      result.append(String.format("%02x", b));
+    }
+    return result.toString();
+  }
+
+  /**
+   * Sets up routes for the `user` collection endpoints.
+   * A UserController instance handles the user endpoints,
+   * and the addRoutes method adds the routes to this controller.
+   *
+   * These endpoints are:
+   *   - `GET /api/users/:id`
+   *       - Get the specified user
+   *   - `GET /api/users?age=NUMBER&company=STRING&name=STRING`
+   *      - List users, filtered using query parameters
+   *      - `age`, `company`, and `name` are optional query parameters
+   *   - `GET /api/usersByCompany`
+   *     - Get user names and IDs, possibly filtered, grouped by company
+   *   - `DELETE /api/users/:id`
+   *      - Delete the specified user
+   *   - `POST /api/users`
+   *      - Create a new user
+   *      - The user info is in the JSON body of the HTTP request
+   *
+   * GROUPS SHOULD CREATE THEIR OWN CONTROLLERS THAT IMPLEMENT THE
+   * `Controller` INTERFACE FOR WHATEVER DATA THEY'RE WORKING WITH.
+   * You'll then implement the `addRoutes` method for that controller,
+   * which will set up the routes for that data. The `Server#setupRoutes`
+   * method will then call `addRoutes` for each controller, which will
+   * add the routes for that controller's data.
+   *
+   * @param server The Javalin server instance
+   */
+  @Override
+  public void addRoutes(Javalin server) {
+    // Get the specified todo
+    server.get(API_TODOS_BY_ID, this::getTodo);
+
+    // List users, filtered using query parameters
+    server.get(API_TODOS, this::getTodos);
+
+    // Add new user with the user info being in the JSON body
+    // of the HTTP request
+    //server.post(API_TODOS, this::addNewTodo);
+
+    // Delete the specified user
+    //server.delete(API_TODOS_BY_ID, this::deleteTodos);
+  }
+}
